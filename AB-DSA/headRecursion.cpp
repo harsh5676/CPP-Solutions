cf668ce66c8f8d50bb007807f6967363e2b1bf8c
harsh5676 <harshithr9804@gmail.com>
2023-11-28 09:30:46 +0530

    28 November, 2023

---
+++ AB-DSA/headRecursion.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+void fun(int n)
+{
+    if (n > 0)
+    {
+        fun(n - 1);
+        cout << n << " ";
+    }
+}
+int main()
+{
+    int x = 3;
+
+    fun(x);
+    return 0;
+}
